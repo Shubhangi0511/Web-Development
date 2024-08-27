75263d9e0a59e389f4d337ddd080d83336efaf53
Shubhangi0511 <singhshubhangi0511@gmail.com>
2024-08-27 21:14:28 +0530

    Web Dev Projects

---
+++ Web Development/THE ECOMMERCE PROJECT/THE ECOMMERCE PROJECT/scripts/productsSp.js
@@ -0,0 +1,123 @@
+// all the products for the random single page products list
+let products = [
+  {
+    name: "Kit Professora",
+    tag: "k1000",
+    price: 270,
+    inCart: 0,
+  },
+  {
+    name: "Lancheira Térmica",
+    tag: "l1001",
+    price: 95,
+    inCart: 0,
+  },
+  {
+    name: "Kit Necessaires",
+    tag: "k1001",
+    price: 170,
+    inCart: 0,
+  },
+  {
+    name: "Lancheira Térmica",
+    tag: "l1002",
+    price: 95,
+    inCart: 0,
+  },
+  {
+    name: "Bolsa Professora Com Repartições",
+    tag: "b1000",
+    price: 170,
+    inCart: 0,
+  },
+  {
+    name: "Necessaires Tamanho P",
+    tag: "n1000",
+    price: 15,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Um Compartimento Branco",
+    tag: "e1000",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Bolsa Professora Com Repartições",
+    tag: "b1001",
+    price: 170,
+    inCart: 0,
+  },
+  {
+    name: "Lancheira Térmica",
+    tag: "l1000",
+    price: 95,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Miney Um Compartimento Vermelho",
+    tag: "e1001",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Mickey Um Compartimento Branco",
+    tag: "e1002",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Mickey Um Compartimento Azul",
+    tag: "e1003",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Mochila Mickey - Bolso 3D Tamanho Grande",
+    tag: "m1000",
+    price: 250,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Estampa Mandala",
+    tag: "n1001",
+    price: 25,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Estampa Flamingo",
+    tag: "n1002",
+    price: 35,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Com Forro Impermeavél",
+    tag: "n1003",
+    price: 50,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Tecido Sintético",
+    tag: "n1004",
+    price: 20,
+    inCart: 0,
+  },
+  {
+    name: "Estojo Um Compartimento Tecido Sintético",
+    tag: "e1004",
+    price: 20,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Multiuso",
+    tag: "n1005",
+    price: 35,
+    inCart: 0,
+  },
+  {
+    name: "Necessaire Animal Print Sintético",
+    tag: "n1006",
+    price: 35,
+    inCart: 0,
+  },
+];
---
+++ Web Development/THE ECOMMERCE PROJECT/THE ECOMMERCE PROJECT/scripts/sproduct.js
@@ -0,0 +1,275 @@
+const carts = document.querySelectorAll(".add-cart");
+const numbers = document.querySelectorAll(".numbers");
+
+carts.forEach((cart, i) => {
+  cart.addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCoast(products[i]);
+  });
+});
+
+function sendMessage() {
+  let cartItems = localStorage.getItem("productsInCart");
+  let productNumbers = localStorage.getItem("cartNumbers");
+  let totalCoast = localStorage.getItem("totalCoast");
+  let formatedProductList = [];
+  let productList;
+  let message;
+
+  cartItems = JSON.parse(cartItems);
+
+  Object.values(cartItems).map((product) => {
+    formatedProductList.push({
+      name: product.name,
+      quantity: product.inCart,
+    });
+  });
+
+  console.log(formatedProductList);
+
+  productList = formatedProductList.toString();
+  checkProductListSize = () => {
+    if (formatedProductList.length > 1) {
+      message = `Olá, estou interessado em compras esses produtos: `;
+      formatedProductList.forEach((product) => {
+        message += `${product.name}, Quantidade: ${product.quantity}\n`;
+      });
+    } else {
+      message = `Olá, estou interessado em comprar esse produto: `;
+      formatedProductList.forEach((product) => {
+        message += `${product.name}, Quantidade: ${product.quantity}\n`;
+      });
+    }
+  };
+  checkProductListSize();
+  var whatsappUrl =
+    "https://api.whatsapp.com/send?phone=+5519995723855&text=" +
+    encodeURIComponent(message);
+  window.open(whatsappUrl);
+
+  localStorage.setItem("productsInCart", JSON.stringify({}));
+  localStorage.setItem("cartNumbers", 0);
+  localStorage.setItem("totalCoast", 0);
+}
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (cartNumbers) {
+    let cartNumbers = document.querySelectorAll(".cart-count");
+    cartNumbers.forEach((cartNumber) => {
+      cartNumber.textContent = productNumbers;
+    });
+  }
+}
+
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    let cartCount = document.querySelectorAll(".cart-count");
+    cartCount.forEach((cart) => {
+      cart.textContent = productNumbers + 1;
+    });
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    let cartCount = document.querySelectorAll(".cart-count");
+    cartCount.forEach((cart) => {
+      cart.textContent = 1;
+    });
+  }
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCoast(product) {
+  let cartCost = localStorage.getItem("totalCoast");
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCoast", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCoast", product.price);
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector("#products");
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+            <tr id = 'product_${item.tag}'>
+                <td>
+                    <a onclick="removeItems('${item.tag}')">
+                        <i class="far fa-times-circle remove-cart"></i>
+                    </a>
+                </td>
+                <td>
+                    <img src="./img/Products/${item.tag}.png" alt="">
+                </td>
+                <td>${item.name}</td>
+                <td>R$${item.price},00</td>
+                <td>
+                    <input onchange="changeCartNumber('${
+                      item.tag
+                    }')" class="numbers" type="number" value="${item.inCart}">
+                </td>
+                <td>R$${item.inCart * item.price},00</td>
+            </tr>`;
+    });
+  }
+}
+
+function removeItems(productTag) {
+  let element = document.getElementById(`product_${productTag}`);
+  let cartItems = localStorage.getItem("productsInCart");
+  let numbersInCart = localStorage.getItem("cartNumbers");
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let total = 0;
+  let totalInCart = 0;
+
+  cartItems = JSON.parse(cartItems);
+
+  Object.values(cartItems).forEach((item) => {
+    if (item.tag == productTag) {
+      total = item.inCart * item.price;
+      totalInCart = item.inCart;
+      item.inCart -= 1;
+      delete cartItems[item.tag];
+    }
+  });
+
+  localStorage.setItem("totalCoast", parseInt(totalCoasts) - total);
+  localStorage.setItem(
+    "cartNumbers",
+    parseInt(numbersInCart) - parseInt(totalInCart)
+  );
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+  element.remove(element);
+
+  calculateTotals();
+  onLoadCartNumbers();
+}
+
+function changeCartNumber(productTag) {
+  let element = document.getElementById(`product_${productTag}`);
+  let cartItems = localStorage.getItem("productsInCart");
+  let numbersInCart = localStorage.getItem("cartNumbers");
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let rawString = element.children[5].textContent;
+  let rawNumber = rawString
+    .replace(/([R$,])/g, "")
+    .slice(0, rawString.length - 5);
+  let productPrice = 0;
+  let productArray = [];
+  let productList = {};
+
+  cartItems = JSON.parse(cartItems);
+
+  Object.values(cartItems).map((product) => {
+    productArray.push(
+      (productList = {
+        name: product.name,
+        quantity: product.inCart,
+      })
+    );
+  });
+
+  Object.values(cartItems).forEach((product) => {
+    if (product.tag == productTag) {
+      productArray.push(
+        (productList = {
+          name: product.name,
+          quantity: product.inCart,
+        })
+      );
+
+      if (product.inCart < element.children[4].lastElementChild.valueAsNumber) {
+        productPrice = product.price;
+        product.inCart += 1;
+
+        localStorage.setItem("cartNumbers", parseInt(numbersInCart) + 1);
+        localStorage.setItem(
+          "totalCoast",
+          parseInt(totalCoasts) + productPrice
+        );
+        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+        rawNumber = Number(rawNumber) + Number(productPrice);
+        element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`;
+        onLoadCartNumbers();
+        calculateTotals();
+      } else if (
+        product.inCart > element.children[4].lastElementChild.valueAsNumber &&
+        element.children[4].lastElementChild.valueAsNumber !== 0
+      ) {
+        productPrice = product.price;
+        product.inCart -= 1;
+
+        localStorage.setItem("cartNumbers", parseInt(numbersInCart) - 1);
+        localStorage.setItem(
+          "totalCoast",
+          parseInt(totalCoasts) - productPrice
+        );
+        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+        rawNumber = Number(rawNumber) - Number(productPrice);
+        element.children[5].textContent = `R$${Number(rawNumber).toFixed(2)}`;
+        onLoadCartNumbers();
+        calculateTotals();
+      } else {
+        removeItems(productTag);
+      }
+    }
+  });
+}
+
+function calculateTotals() {
+  let totalCoasts = localStorage.getItem("totalCoast");
+  let subTotalContainer = document.querySelector("#total-data");
+  let cartItems = localStorage.getItem("productsInCart");
+
+  cartItems = JSON.parse(cartItems);
+
+  if (subTotalContainer) {
+    subTotalContainer.innerHTML = "";
+    subTotalContainer.innerHTML = `
+      <tr>
+          <td>Cart Subtotal</td>
+          <td>R$ ${totalCoasts},00</td>
+      </tr>
+      <tr>
+          <td>Shipping</td>
+          <td>Free</td>
+      </tr>
+      <tr>
+          <td><strong>Total</strong></td>
+          <td><strong>R$ ${totalCoasts},00</strong></td>
+      </tr>
+      `;
+  }
+}
+
+calculateTotals();
+onLoadCartNumbers();
+displayCart();
